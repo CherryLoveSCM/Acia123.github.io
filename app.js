e4de54fc33d94d6cc13a9f49183919441617650f
Acia123 <instareg10@mail.com>
2016-08-19 19:06:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-var main =  function() {
-  $(".bild2").click(function(){
-    $(".meny").animate({
+ar main = function() {
+  /* Push the body and the nav over by 285px over */
+  $('.bild2').click(function() {
+    $('.meny').animate({
       left: "0px"
-  }, 200);
-  $("body").animate({
-    left: "300px"
-  }, 200);
-  });
-  $(".bild3").click(function(){
-    $(".meny").animate({
-  left: "-300px"
     }, 200);
-    $("body").animate({
+
+    $('body').animate({
+      left: "285px"
+    }, 200);
+  });
+
+  /* Then push them back */
+  $('.bild3').click(function() {
+    $('.meny').animate({
+      left: "-285px"
+    }, 200);
+
+    $('body').animate({
       left: "0px"
     }, 200);
   });
 };
+
+
 $(document).ready(main);
