d2effa4a50f27d11ef63825120f69b5874dcd9a2
Acia123 <instareg10@mail.com>
2016-08-19 19:50:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openMeny() {
-    document.getElementByClassName("meny").style.width = "250px";
-    document.getElementByClassName("main").style.marginLeft = "250px";
-}
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-function closeMeny() {
-    document.getElementByClassName("meny").style.width = "0";
-    document.getElementByClassName("main").style.marginLeft = "0";
-}
+var main =  function() {
+  $(".bild2").click(function(){
+    $(".meny").animate({
+      left: "0px"
+  }, 200);
+  $("body").animate({
+    left: "300px"
+  }, 200);
+  });
+  $(".bild3").click(function(){
+    $(".meny").animate({
+  left: "-300px"
+    }, 200);
+    $("body").animate({
+      left: "0px"
+    }, 200);
+  });
+};
+$(document).ready(main);
