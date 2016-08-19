1a83c4d7252879dfcae8d369b828093b169d6f8c
Acia123 <instareg10@mail.com>
2016-08-19 18:50:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ var main =  function() {
   $(".bild2").click(function(){
     $(".meny").animate({
       left: "0px"
-  }, 300);
+  }, 200);
   $("body").animate({
-    left: "285px"
-  }, 300);
+    left: "300px"
+  }, 200);
   });
   $(".bild3").click(function(){
     $(".meny").animate({
-  left: "-285px"
-    }, 300);
+  left: "-300px"
+    }, 200);
     $("body").animate({
       left: "0px"
-    }, 300);
+    }, 200);
   });
 };
 $(document).ready(main);
